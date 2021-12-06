cb7d93f0b2dac53cd2bcbe07f5e8309c8ca4faa8
Krzysztof Ostrowski <ostrowski021@gmail.com>
2021-12-07 00:58:57 +0100

    First version of RectangleCreator added

---
+++ SimpleRectangleCreator/SimpleRectangleCreator/Program.cs
@@ -0,0 +1,19 @@
+﻿//Display rectangle in console with height and width shown in "*"
+
+
+Console.WriteLine("Enter rectangle height:");
+var height = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter rectangle width:");
+var width = int.Parse(Console.ReadLine());
+
+
+for (int j = 0; j < height; j++)
+{
+    for (int i = 0; i < width; i++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine("");
+}
+
+Console.ReadKey();
