dda2f103109f968ce44e4419966c356d0b1e10c8
Krzysztof Ostrowski <ostrowski021@gmail.com>
2021-12-07 01:39:59 +0100

    Color and play again option added

--- SimpleRectangleCreator/SimpleRectangleCreator/Program.cs
+++ SimpleRectangleCreator/SimpleRectangleCreator/Program.cs
@@ -1,6 +1,7 @@
 ﻿//Display rectangle in console with height and width shown in "*"
 
-
+Start:
+Console.ForegroundColor = ConsoleColor.Green;
 Console.WriteLine("Enter rectangle height:");
 var height = int.Parse(Console.ReadLine());
 Console.WriteLine("Enter rectangle width:");
@@ -16,4 +17,22 @@ for (int j = 0; j < height; j++)
     Console.WriteLine("");
 }
 
-Console.ReadKey();
+Console.WriteLine("Would you like to create rectangle again?");
+Console.WriteLine("Press 'y' if yes and 'n' if no: ");
+Decision:
+var decision = Console.ReadKey(true);
+
+if (decision.KeyChar == 'y')
+{
+    goto Start;
+}
+else if (decision.KeyChar=='n')
+{
+    goto End;
+}
+else
+{
+    Console.WriteLine("wrong key :(");
+    goto Decision;
+}
+End: ;
